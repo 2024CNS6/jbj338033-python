b1fb7306a9c9a384623f816a5670e56de3af9216
전민오 <jbj338033@gmail.com>
2024-06-17 19:50:01 +0900

    Baekjoon 2024/06/08

--- baekjoon/1074.py
+++ baekjoon/1074.py
@@ -1,1 +1,12 @@
 n,r,c=map(int,input().split())
+
+def f(n,r,c):
+    if n==0:return 0
+    m=2**(n-1)
+    if r<m:
+        if c<m:return f(n-1,r,c)
+        else:return m*m+f(n-1,r,c-m)
+    else:
+        if c<m:return 2*m*m+f(n-1,r-m,c)
+        else:return 3*m*m+f(n-1,r-m,c-m)
+print(f(n,r,c))
--- baekjoon/10992.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-
-for i in range(n):
-    if i==0:
-        print(' '*(n-i-1)+'*'+' '*(2*i-1)+'*'*(i!=0))
-    elif i==n-1:
-        print('*'*(2*n-1))
-    else:
-        print(' '*(n-i-1)+'*'+' '*(2*i-1)+'*'+' '*(n-i-1))
